46e51fb7e6ceb248a9026c8f459efd726cae52a8
Võ Cao Trí <trivo.131003@gmail.com>
2025-03-13 20:38:34 +0700

    database url for deploying

--- config.py
+++ config.py
@@ -1,6 +1,5 @@
 import os
 
 class Config:
-    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL', 'postgresql://user:strongpassword@db:5432/exam_db')
+    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL', 'postgresql://user:vE1lyUShe8iqdnWwCY1MEmpXq9sG4P5T@dpg-cv99j4hc1ekc73e5l4u0-a.singapore-postgres.render.com/db_hztz')
 
-##postgresql://user:vE1lyUShe8iqdnWwCY1MEmpXq9sG4P5T@dpg-cv99j4hc1ekc73e5l4u0-a.singapore-postgres.render.com/db_hztz
